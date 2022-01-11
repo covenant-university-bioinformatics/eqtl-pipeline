cb2f81cd5b19c6bccf2e8c4c530ed4579d0e9779
yagoubali <yagoubali@gmail.com>
2022-01-11 10:09:29 -0300

    Manhattan plot title

--- scripts/plot_qq_manhattan.R
+++ scripts/plot_qq_manhattan.R
@@ -24,11 +24,12 @@ output_prefix <- args[3]
 GWAS_summary=read.table(GWAS_summary,header=T)
 qq=paste0(output_dir,"/", output_prefix, "_","qq.png")
 png(qq)# width=950, height=500)
-qq(GWAS_summary$p_SMR, main = "Q-Q plot of GWAS p-values")
+qq(GWAS_summary$p_SMR, main = "Q-Q plot of SMR p-values")
 dev.off()
 manhattan =paste0(output_dir,"/", output_prefix, "_", "manhattan.png")
 png(manhattan, width=1000, height=500)
-manhattan(GWAS_summary, chr="topSNP_chr", bp="topSNP_bp", snp="topSNP", p="p_SMR", col = c("blue4", "orange3"), suggestiveline = F)
+manhattan(GWAS_summary, chr="topSNP_chr", bp="topSNP_bp", snp="topSNP", p="p_SMR", col = c("blue4", "orange3"), suggestiveline = F,
+main="Manhattan plots of SMR tests for association between gene expression and complex traits")
 dev.off()
 
 ##probes
@@ -36,3 +37,5 @@ dev.off()
 #png(manhattan_probes, width=1000, height=500)
 #manhattan(GWAS_summary, chr="ProbeChr", bp="Probe_bp", snp="probeID", p="p_SMR" )
 #dev.off()
+quit(save = "no", status = 0, runLast = FALSE)
+
